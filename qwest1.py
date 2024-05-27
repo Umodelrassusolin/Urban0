d2da59266d09aa40d71ae6ec8c422b891fa70540
Umodelrassusolin <umodelrassusolin@gmail.com>
2024-05-27 20:04:53 +0300

    Add files via upload

---
+++ qwest1.py
@@ -0,0 +1,36 @@
+#Длина слова
+
+a='Ламповый проигрыватель'
+print(a)
+print('Длина слова: ',a,' = ',len(a))
+
+#Суммы и разности
+
+first=2
+second=7
+summa=first+second
+diff=first-second
+print('Суммы и разницы: ',summa,' ',diff)
+
+#Среднее арифметическое
+
+first=4
+second=22
+therd=17
+mean1=(first+second+therd)/3
+mean2=int(first+second+therd)/3
+print('Среднее арифметическое: ',mean1,mean2)
+
+#Простые строчки
+
+first_string='Понедельник'
+second_string='Вторник'
+print('Простые строки: ',first_string,', ',second_string)
+
+#Сложная формула
+
+a=7
+b=2
+c=8
+f=((a*b)+(a*c))**3/2
+print('Сложная формула: ',f)
